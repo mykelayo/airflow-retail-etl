db3592629bb77eea6af9aa238ea11d57c4de3805
Michael Bello <56143067+mykelayo@users.noreply.github.com>
2025-05-26 01:04:05 +0000

    Update devcontainer for Docker-in-Docker

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-curl -sSL https://install.astronomer.io | sudo bash
+apk add --no-cache python3 py3-pip
+pip install --upgrade pip
+curl -sSL https://install.astronomer.io | bash
 pip install apache-airflow==2.8.0
 astro dev init
